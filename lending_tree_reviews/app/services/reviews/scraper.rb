ce6a5ed315a7bc75e428e2dea606960585ed05e9
Sam Tucker <samueljeffersontucker@gmail.com>
2019-06-12 19:27:14 -0500

    Refactor scraper.rb to clean up scrape_reviews method

--- lending_tree_reviews/app/services/reviews/scraper.rb
+++ lending_tree_reviews/app/services/reviews/scraper.rb
@@ -13,27 +13,9 @@ module Reviews
 		def scrape_reviews
 			while @more_reviews do
 				@page_number += 1
+				
 				request_page
-
-				#put into method that returns set of reviews and call here to push into array
-				review_panels.each do |review|
-					consumer = review.css('.consumerName').text.split('from')
-					review_points = review.css('.reviewPoints')
-
-					rev = {	
-						title: review.css('.reviewTitle').text.strip,
-						body: review.css('.reviewText').text.strip,
-						rating: review.css('.numRec').text.strip,
-						author: consumer[0].strip,
-						location: consumer[1].strip,
-						date: review.css('.consumerReviewDate').text.split('in')[1].strip,
-						closedWithLender: review_points.css('.yes') ? 'Yes' : 'No',
-						loanType: review_points.css('.loanType')[0].text.strip,
-						reviewType: review_points.css('.loanType')[1].text.strip
-					}
-
-					@reviews << rev
-				end
+				@reviews << reviews_set
 
 				print @page_number
 			end
@@ -46,11 +28,35 @@ module Reviews
 			@html = Nokogiri::HTML.parse(open(url, :ssl_verify_mode => OpenSSL::SSL::VERIFY_NONE))
 
 			@more_reviews = false unless @html.css('.lenderNav').at('a:contains("Next")')
-			#@more_reviews = false if @page_number == 20
+			@more_reviews = false if @page_number == 20
 		end
 
 		def review_panels
 			@html.css('.mainReviews')
+		end
+
+		def reviews_set
+			set = []
+			review_panels.each do |review|
+				consumer = review.css('.consumerName').text.split('from')
+				review_points = review.css('.reviewPoints')
+
+				rev = {	
+					title: review.css('.reviewTitle').text.strip,
+					body: review.css('.reviewText').text.strip,
+					rating: review.css('.numRec').text.strip,
+					author: consumer[0].strip,
+					location: consumer[1].strip,
+					date: review.css('.consumerReviewDate').text.split('in')[1].strip,
+					closedWithLender: review_points.css('.yes') ? 'Yes' : 'No',
+					loanType: review_points.css('.loanType')[0].text.strip,
+					reviewType: review_points.css('.loanType')[1].text.strip
+				}
+
+				set << rev
+			end
+
+			set
 		end
 	end
 end
